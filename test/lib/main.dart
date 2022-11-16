f10636bcdafeced25d70a0030041798138816f83
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2022-11-16 09:38:04 +0200

    firebase steps

---
+++ test/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase"),
+      ),
+      body: const Center(
+          child: Text(
+        "Firebase",
+        style: TextStyle(fontSize: 50, color: Colors.blue),
+      )),
+    );
+  }
+}
